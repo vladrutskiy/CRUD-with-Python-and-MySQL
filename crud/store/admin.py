2ece2d5cb5a239115385ecfc7cb042a9ceef752e
Vlad Rutskiy <vladrytskiy@gmail.com>
2025-02-19 14:26:33 -0700

    1. Added Album grid 2. Added Sales Persons grid 3. Added Tracks grid

--- crud/store/admin.py
+++ crud/store/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import SalesPersons, Track, Album
+
 
 # Register your models here.
+@admin.register(SalesPersons)
+class SalesPersonsAdmin(admin.ModelAdmin):
+    list_display = ('First_Name', 'Last_Name', 'City', 'Sales')
+
+@admin.register(Track)
+class TrackAdmin(admin.ModelAdmin):
+    list_display = ('TrackId', 'Name', 'AlbumId', 'MediaTypeId', 'GenreId', 'Composer', 'Milliseconds', 'UnitPrice')
+
+@admin.register(Album)
+class AlbumAdmin(admin.ModelAdmin):
+    list_display = ('AlbumId', 'Title', 'ArtistId')    
--- crud/store/models.py
+++ crud/store/models.py
@@ -1,3 +1,57 @@
 from django.db import models
 
 # Create your models here.
+# Connecting existing empty Table SalesPersons
+class SalesPersons(models.Model):
+    First_Name = models.CharField(max_length=30)
+    Last_Name = models.CharField(max_length=30)
+    City = models.CharField(max_length=30)
+    Sales = models.CharField(max_length=30)
+
+#  Showing desired cpolumns
+    def __str__(self):
+        return (f"{self.First_Name} {self.Last_Name} {self.City} {self.Sales} ")
+    
+# telling not to create a new grid but connect to the existing one
+    class Meta:
+        db_table = 'SalesPersons'  # Map to the existing table
+        managed = False  # Tell Django not to manage this table
+
+
+# Connecting not empty Album table
+class Album(models.Model):
+    AlbumId = models.SmallIntegerField(primary_key=True)
+    Title = models.CharField(max_length=35)
+    ArtistId = models.SmallIntegerField()
+    
+
+#  Showing desired cpolumns
+    def __str__(self):
+        return (f"{self.AlbumId} {self.Title} {self.ArtistId} ")
+    
+# telling not to create a new grid but connect to the existing one
+    class Meta:
+        db_table = 'Album'  # Map to the existing table
+        managed = False  # Tell Django not to manage this table        
+
+
+# Connecting not empty Track table
+class Track(models.Model):
+    TrackId = models.SmallIntegerField(primary_key=True)
+    Name = models.CharField(max_length=24)
+    AlbumId = models.SmallIntegerField()
+    MediaTypeId = models.SmallIntegerField()
+    GenreId = models.SmallIntegerField()
+    Composer = models.CharField(max_length=35)
+    Milliseconds  = models.CharField(max_length=7)
+    Bytes = models.BigIntegerField()
+    UnitPrice = models.DecimalField(max_digits=10, decimal_places=2)
+
+#  Showing desired cpolumns
+    def __str__(self):
+        return (f"{self.TrackId} {self.Name} {self.AlbumId} {self.MediaTypeId} {self.GenreId} {self.Composer} {self.Milliseconds} {self.UnitPrice} ")
+    
+# telling not to create a new grid but connect to the existing one
+    class Meta:
+        db_table = 'Track'  # Map to the existing table
+        managed = False  # Tell Django not to manage this table
