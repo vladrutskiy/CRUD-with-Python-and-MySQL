9d436c9f62d05af30cf19629c5efcf4cb3c84e71
Vlad Rutskiy <vladrytskiy@gmail.com>
2025-02-20 21:06:42 -0700

    Fixed all image paths on the old website

--- crud/crud/settings.py
+++ crud/crud/settings.py
@@ -126,6 +126,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/crud/store/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",  # Global static directory
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
